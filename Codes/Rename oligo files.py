84d1070d455ef253b2c455feff2bbc959c8901d7
maxiangzju <36394525+maxiangzju@users.noreply.github.com>
2018-12-14 22:40:06 -0800

    Create Rename oligo files.py

---
+++ Codes/Rename oligo files.py
@@ -0,0 +1,21 @@
+newfile = open('newfile.tab', 'w')
+less2 = open('lessthan2.tab', 'w')
+
+fhand = open('96well-oligo.tab')
+for line in fhand:
+    line = line.strip()
+    pool,well,num,gene,null,seq = line.split('\t')
+    
+    if gene != 'Blank':
+        gene = 'TET.' + gene
+        if len(gene) > 30:
+            gene = gene[:15] + gene[-15:]
+        
+    new_line = '\t'.join([pool,well,num,gene,null,seq])
+    print(new_line, file=newfile)
+    
+    if seq.count('N') < 2 and gene != 'Blank':
+        print(new_line, file=less2)
+
+newfile.close()
+less2.close()
