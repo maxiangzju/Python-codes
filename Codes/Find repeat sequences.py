c350bf093893bd0dd365b485aea09261682ea6b0
maxiangzju <36394525+maxiangzju@users.noreply.github.com>
2018-12-16 17:11:24 -0800

    Update Find repeat sequences.py

--- Codes/Find repeat sequences.py
+++ Codes/Find repeat sequences.py
@@ -1,36 +1,47 @@
-import sys
-import getopt
+import argparse
 from Bio import SeqIO
 
-opt, arg = getopt.getopt(sys.argv[1:], 'i:o:')
-opts = dict()
-for option, value in opt:
-    opts[option] = value
+'''
+For a given fasta file, find the repeats of given number 'n' of bases with the specified
+occurrence number 'frequence' 
+'''
 
-if '-i' in opts.keys():
-    inname = opts['-i']
-if '-o' in opts.keys():
-    outname = opts['-o']
+def find_repeats(infile, outfile, n, frequence):
+    try:
+        records = list(SeqIO.parse(infile, 'fasta'))
+    except:
+        print('File not found!')
+        return
 
-try:
-    records = list(SeqIO.parse(inname, 'fasta'))
-except:
-    print('File not found!')
+    outfile = open(outfile, 'w')
 
-outfile = open(outname, 'w')
-
-list_query = list()
-for r in records:
-    for i in range(len(r)):
-        if len(r.seq[i: i+7]) < 7: continue
-        list_query.append(str(r.seq[i: i+7]))
-query = set(list_query)
-
-count = dict()
-for q in query:
-    num = 0
+    list_query = list()
     for r in records:
-        num += r.seq.count_overlap(q) 
-    count[q] = num
-print(sorted([(v,k) for k,v in count.items() if v > 30], reverse = True), file=outfile)
- 
+        for i in range(len(r)):
+            if len(r.seq[i: i+n]) < n: continue
+            list_query.append(str(r.seq[i: i+n]))
+    query = set(list_query)
+
+    count = dict()
+    for q in query:
+        num = 0
+        for r in records:
+            num += r.seq.count_overlap(q)
+        count[q] = num
+    print(sorted([(v,k) for k,v in count.items() if v > frequence], reverse = True), file=outfile)
+    outfile.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Find repeats in given fasta files')
+    parser.add_argument('-i', '--input', metavar='', required=True, help='input fasta file')
+    parser.add_argument('-o', '--output', metavar='', required=True, help='output sequence with occurrence file')
+    parser.add_argument('-f', '--frequence', type=int, metavar='', required=True, help='repeat sequence frequence')
+    parser.add_argument('-n', '--numberbase', type=int, metavar='', required=True, help='number of bases of repeat sequences')
+    args = parser.parse_args()
+
+    infile = args.input
+    outfile = args.output
+    frequence = args.frequence
+    n = args.numberbase
+
+    find_repeats(infile, outfile, n, frequence)
