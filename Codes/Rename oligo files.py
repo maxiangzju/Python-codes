e13cf3ebcdb92c41d996c93209839de098786d0e
maxiangzju <36394525+maxiangzju@users.noreply.github.com>
2018-12-16 20:58:10 -0800

    Update Rename oligo files.py

--- Codes/Rename oligo files.py
+++ Codes/Rename oligo files.py
@@ -1,21 +1,39 @@
-newfile = open('newfile.tab', 'w')
-less2 = open('lessthan2.tab', 'w')
+import argparse
 
-fhand = open('oligo.tab')
-for line in fhand:
-    line = line.strip()
-    pool,well,num,gene,null,seq = line.split('\t')
-    
-    if gene != 'Blank':
-        gene = 'TET.' + gene
-        if len(gene) > 30:
-            gene = gene[:15] + gene[-15:]
-        
-    new_line = '\t'.join([pool,well,num,gene,null,seq])
-    print(new_line, file=newfile)
-    
-    if seq.count('N') < 2 and gene != 'Blank':
-        print(new_line, file=less2)
+'''
+Rename oligo name from primer files, if oligo name longer than 30, rename the oligo
+to generate a new unique name
+'''
 
-newfile.close()
-less2.close()
+def rename(infile, outfile):
+
+    outfile = open(outfile, 'w')
+    infhand = open(infile)
+
+    for line in infhand:
+        line = line.strip()
+        group,well,num,gene,null,seq = line.split('\t')
+
+        if gene != 'Blank':
+            gene = 'TET.' + gene
+            if len(gene) > 30:
+                gene = gene[:15] + gene[-15:]
+
+        new_line = '\t'.join([group,well,num,gene,null,seq])
+        print(new_line, file=outfile)
+
+    infhand.close()
+    outfile.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+                    Rename oligo name from primer files, if oligo name longer than 30,
+                    rename the oligo to generate a new unique name
+                    ''')
+    parser.add_argument('-i', '--input', metavar='', required=True, help='input oligo file')
+    parser.add_argument('-o', '--output', metavar='', required=True, help='output oligo file')
+    args = parser.parse_args()
+
+    infile = args.input
+    outfile = args.output
+    rename(infile, outfile)
